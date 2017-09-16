526e7d9abe44afd8c2513100bb061e5a47fd9116
aysonsteven <aysonsteven@gmail.com>
2017-09-16 11:47:26 +0800

    global styles

--- src/pages/pagenotfound/notfound.page.ts
+++ src/pages/pagenotfound/notfound.page.ts
@@ -1,7 +1,36 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { Router } from "@angular/router"
 @Component({
     selector: 'notfound',
-    template: `Page not found`,
-    styles: [ `` ]
+    template: `
+    <div>
+        <h1><h1>{{ returnRoute() }}</h1> page does not exist</h1>
+    </div>
+    `,
+    styles: [ `
+        div{
+            text-align: center;
+        }
+    div  h1 {
+        color: #2aabd2;
+        display: inline-block;
+        
+    }
+        h1 > h1{
+            text-transform: capitalize;
+        }
+    ` ]
 })
-export class NotfoundPage{}
+export class NotfoundPage implements OnInit{
+
+    constructor( private activeRoute: Router ){}
+
+    ngOnInit(){
+        console.log('route', this.activeRoute.url )
+    }
+
+    returnRoute(){
+        return this.activeRoute.url.replace('/', '');
+    }
+
+}
